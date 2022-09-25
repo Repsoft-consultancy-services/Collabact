50e8d9dfca35367c7aebc74ff3cf538ae1c11e47
Yeasir-Hossain <yeasir06@gmail.com>
2022-09-25 22:44:11 +0600

    changes

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -18,22 +18,22 @@ const Navbar = () => {
             setServicedrop(false)
     }
     // const menu = <>
-    //     <h1><Link href='/'><p className='bg-white text-base xl:text-xl text-[#717171]'>Home</p></Link></h1>
-    //     <h1><Link href='/joinUs'><p className='bg-white text-base xl:text-xl text-[#717171]'>AboutUs</p></Link></h1>
+    //     <h1><Link href='/'><p className='bg-white  font-medium text-base xl:text-xl text-[#717171]'>Home</p></Link></h1>
+    //     <h1><Link href='/joinUs'><p className='bg-white  font-medium text-base xl:text-xl text-[#717171]'>AboutUs</p></Link></h1>
     //     <h1 onClick={handledropdown}>
-    //         <p className='bg-white text-base xl:text-xl text-[#717171]'>What we offer</p>
+    //         <p className='bg-white  font-medium text-base xl:text-xl text-[#717171]'>What we offer</p>
     //     </h1>
     //     <Link href="/">
     //         <div className=''>
     //             <Image src={CA} alt=""></Image>
     //         </div>
     //     </Link>
-    //     <h1><Link href='/contactUs'><p className='bg-white text-base xl:text-xl text-[#717171]'>Blog</p></Link></h1>
-    //     <h1><Link href='/contactUs'><p className='bg-white text-base xl:text-xl text-[#717171]'>FAQ</p></Link></h1>
-    //     <h1><Link href='/aboutUs'><p className='bg-white text-base xl:text-xl '>Contact Us</p></Link></h1>
+    //     <h1><Link href='/contactUs'><p className='bg-white  font-medium text-base xl:text-xl text-[#717171]'>Blog</p></Link></h1>
+    //     <h1><Link href='/contactUs'><p className='bg-white  font-medium text-base xl:text-xl text-[#717171]'>FAQ</p></Link></h1>
+    //     <h1><Link href='/aboutUs'><p className='bg-white  font-medium text-base xl:text-xl '>Contact Us</p></Link></h1>
     // </>
     return (
-        <div className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} my-2`}>
+        <div className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} my-5`}>
             <div className="w-full lg:hidden mt-1">
                 <div className='flex justify-between items-center pr-2'>
                     <div className="dropdown">
@@ -48,15 +48,15 @@ const Navbar = () => {
                         </label>
                         <div tabIndex="0" className={`menu menu-compact ${menudrop ? 'dropdown-content' : 'hidden'} mt-3 p-2 shadow
                      rounded-md w-52 bg-white`}>
-                            <Link href='/'><h1 onClick={() => setMenudrop(false)} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Home</h1></Link>
-                            <Link href='/aboutUs'><h1 onClick={() => setMenudrop(false)} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/aboutUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>AboutUs</h1></Link>
+                            <Link href='/'><h1 onClick={() => setMenudrop(false)} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Home</h1></Link>
+                            <Link href='/aboutUs'><h1 onClick={() => setMenudrop(false)} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/aboutUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>AboutUs</h1></Link>
 
-                            <h1 onTouchStart={handledropdown} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer</h1>
-                            <Link href='https://collabact.com/Blog/'><h1 onClick={() => setMenudrop(false)} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/blog' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Blog</h1></Link>
+                            <h1 onTouchStart={handledropdown} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer</h1>
+                            <Link href='https://collabact.com/Blog/'><h1 onClick={() => setMenudrop(false)} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/blog' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Blog</h1></Link>
 
-                            <Link href='/faq'><h1 onClick={() => setMenudrop(false)} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/faq' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>FAQ</h1></Link>
+                            <Link href='/faq'><h1 onClick={() => setMenudrop(false)} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/faq' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>FAQ</h1></Link>
 
-                            <Link href='/contactUs'><h1 onClick={() => setMenudrop(false)} className={`bg-white text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/contactUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Contact Us</h1></Link>
+                            <Link href='/contactUs'><h1 onClick={() => setMenudrop(false)} className={`bg-white  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/contactUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Contact Us</h1></Link>
                         </div>
                     </div>
                     <Link href="/">
@@ -75,11 +75,11 @@ const Navbar = () => {
             </div>
             <div className="max-w-[110rem] mx-auto mt-1 hidden lg:block">
                 <div className={`flex justify-evenly items-center ml-24`}>
-                    <Link href='/'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Home</h1></Link>
+                    <Link href='/'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black font-medium text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Home</h1></Link>
 
-                    <Link href='/aboutUs'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/aboutUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>AboutUs</h1></Link>
+                    <Link href='/aboutUs'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/aboutUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>AboutUs</h1></Link>
 
-                    <h1 onMouseEnter={handledropdown} className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer</h1>
+                    <h1 onMouseEnter={handledropdown} className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer</h1>
 
                     <Link href="/">
                         <div className=''>
@@ -88,11 +88,11 @@ const Navbar = () => {
                             }
                         </div>
                     </Link>
-                    <Link href='https://collabact.com/Blog/'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/blog' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Blog</h1></Link>
+                    <Link href='https://collabact.com/Blog/'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/blog' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Blog</h1></Link>
 
-                    <Link href='/faq'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl hover:cursor-pointer 2xl:text-lg pb-2 ${route === '/faq' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>FAQ</h1></Link>
+                    <Link href='/faq'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl hover:cursor-pointer 2xl:text-lg pb-2 ${route === '/faq' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>FAQ</h1></Link>
 
-                    <Link href='/contactUs'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/contactUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Contact Us</h1></Link>
+                    <Link href='/contactUs'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl 2xl:text-lg pb-2 hover:cursor-pointer ${route === '/contactUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>Contact Us</h1></Link>
 
                     <div className='space-x-1 pb-2 text-[1rem] text-white'>
                         <Link href='https://app.collabact.com/Account/SignIn'><button className='bg-blue-500 px-1 lg:px-2 py-1 hover:shadow-[0px_3px_20px_0_rgba(0,0,0,0.30)] hover:shadow-blue-600 rounded-lg'>Sign In</button></Link>
@@ -100,7 +100,7 @@ const Navbar = () => {
                     </div>
                 </div>
             </div >
-            {servicedrop && <div onMouseLeave={handledropdown} className="mt-3 rounded-[10px] dropclip absolute top-[150px] lg:top-[59px] left-[1rem] lg:left-[11.5rem] xl:left-[22.5rem] 2xl:left-[37.5rem] 3xl:left-[57.5rem] z-50 px-5 py-3 bg-gray-100 space-y-1 text-base">
+            {servicedrop && <div onMouseLeave={handledropdown} className="mt-3 rounded-[10px] dropclip absolute top-[150px] lg:top-[59px] left-[1rem] lg:left-[11.5rem] xl:left-[22.5rem] 2xl:left-[37.5rem] 3xl:left-[57.5rem] z-50 px-5 py-3 bg-gray-100 space-y-1  font-medium text-base">
                 <div onClick={handledropdown} className="">
                     <h1 className='hover:cursor-pointer'><Link href='/services'><p className='font-semibold uppercase'>Businesses</p></Link></h1>
                 </div>
