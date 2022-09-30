5881edb3775893b7f3bcdd954f433eddd469be49
Yeasir-Hossain <yeasir06@gmail.com>
2022-09-30 15:23:36 +0600

    changes in icons and business grid 4 to 3

--- pages/components/Home/Encash1.js
+++ pages/components/Home/Encash1.js
@@ -14,14 +14,14 @@ const Encash1 = () => {
         },
         {
             "logo": "/speedforce.png",
-            "title": "SpeedForce Ventures Pvt. Ltd",
+            "title": "SpeedForce Ventures Pvt...",
             "product": ["Two Wheeler servicing..."],
             "Segment": "Franchise",
             "Earning": "35000-80000"
         },
         {
             "logo": "/panache.png",
-            "title": "Panache Green Tech Solution...",
+            "title": "Panache Green Tech...",
             "product": ["CoolTops & WaterProofing"],
             "Segment": "Building Construction...",
             "Earning": "25000-60000"
@@ -61,22 +61,29 @@ const Encash1 = () => {
             "Segment": "Franchise",
             "Earning": "21000-70000"
         },
+        {
+            "logo": "/SUGAR.png",
+            "title": "SugarCo",
+            "product": ["Coconut Sugar"],
+            "Segment": "FMCG",
+            "Earning": "10000-40000"
+        },
     ]
     return (
-        <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 py-5 px-2'>
+        <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3 py-5 px-2'>
             {
-                data.map((d, index) => <div key={index} className='bg-white lg:w-[320px] shadow-[0px_0px_8px_0_rgba(0,0,0,0.30)] hover:shadow-[0px_0px_8px_0_rgba(0,0,0,0.50)] shadow-gray-500 hover:shadow-gray-600 hover:cursor-pointer'>
+                data.map((d, index) => <div key={index} className='bg-white lg:w-[448px] shadow-[0px_0px_8px_0_rgba(0,0,0,0.30)] hover:shadow-[0px_0px_8px_0_rgba(0,0,0,0.50)] shadow-gray-500 hover:shadow-gray-600 hover:cursor-pointer'>
                     <div>
                         <div className='flex items-center justify-center'>
-                            <div className='px-11 py-2 my-2 bg-slate-100'>
-                                <Image width={218} height={152} src={d.logo} alt=""></Image>
+                            <div className='px-10 py-2 my-2 bg-slate-100'>
+                                <Image width={350} height={250} src={d.logo} alt=""></Image>
                             </div>
                         </div>
                         <div className='pl-1'>
                             <h6 className='text-[17px] text-[#757575]'>{d.Segment}</h6>
-                            <h1 className='text-base xl:text-[30px] font-bold text-left mt-3'>{d.title}</h1>
-                            <h6 className='text-[22px] mt-5'>Business Consultancy</h6>
-                            <div className='text-[20px] flex justify-between mt-3'>
+                            <h1 className='text-base xl:text-[30px] font-bold text-left mt-5'>{d.title}</h1>
+                            <h6 className='text-[22px] mt-3'>Business Consultancy</h6>
+                            <div className='text-[20px] flex justify-between mt-5'>
                                 <h6 className='font-medium flex-1'>Earning<span className='ml-1'>Range</span></h6>
                                 <div className='ml-6'>
                                     <p>Rs. {d.Earning}/month</p>
@@ -84,7 +91,7 @@ const Encash1 = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='p-2'>
+                    <div className='p-2 mt-3'>
                         <Link href="https://app.collabact.com/Account"><button className='px-5 py-2 bg-[#F5F5F5] w-full'><p className='font-medium text-3xl text-[#757575] uppercase'>know more</p></button></Link>
                     </div>
                 </div>)
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -45,39 +45,39 @@ const Navbar = () => {
                     <ul id="mobileMenu" style={{ display: mobmenu ? "block" : "none" }}>
                         <span></span>
                         <Link href="/">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>Home</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>Home</li>
                         </Link>
                         <Link href="/services">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>Businesses</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>Businesses</li>
                         </Link>
                         <Link href="/services2">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>Sales Partner</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>Sales Partner</li>
                         </Link>
                         <Link href="/aboutUs">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>About Us</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>About Us</li>
                         </Link>
                         <Link href="/faq">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>FAQ</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>FAQ</li>
                         </Link>
                         <Link href="https://collabact.com/Blog/">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>Blog</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>Blog</li>
                         </Link>
                         <Link href="/contactUs">
-                            <li onClick={()=>{
-                            setMobmenu(false)
-                        }}>Contact</li>
+                            <li onClick={() => {
+                                setMobmenu(false)
+                            }}>Contact</li>
                         </Link>
                     </ul>
                 </i>
@@ -100,7 +100,7 @@ const Navbar = () => {
 
                     <Link href='/aboutUs'><h1 className={`${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl  pb-2 hover:cursor-pointer ${route === '/aboutUs' ? 'border-b-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>AboutUs</h1></Link>
 
-                    <div id="ourOfferMenu" className={`ondesktop ${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer
+                    <div id="ourOfferMenu" className={`ondesktop ${route === '/terms' ? 'bg-[#C2C2C3]' : route === '/cookie' ? 'bg-[#C2C2C3]' : route === '/faq' ? 'bg-[#C2C2C3]' : 'bg-white'} hover:text-black  font-medium text-base xl:text-xl pb-2 hover:cursor-pointer ${route === '/services' ? 'border-t-4 border-[#717171] text-black font-bold' : route === '/services2' ? 'border-t-4 border-[#717171] text-black font-bold' : 'text-[#717171]'}`}>What we offer
                         <div className="mt-3 offerList rounded-[10px] dropclip bg-gray-100 font-medium py-2 text-base w-44">
                             <div className='hover:bg-gray-300'>
                                 <h1 className='hover:cursor-pointer py-1 px-5'><Link href='/services'><p className='font-semibold uppercase'>Businesses</p></Link></h1>
